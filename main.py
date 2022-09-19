ffdf5f2c9fef85d8c8ee4e5c046e7ce8088c9c68
JasmineUsamaHegazy <yuhijazy@gmail.com>
2022-09-19 23:34:41 +0300

    Q2

---
+++ main.py
@@ -0,0 +1,27 @@
+n = input("Number1,Number2")
+n1 = input("Numer1")
+value= int(n1)
+n2 = input("Number2")
+value2 = int(n2)
+result = n1 + n2
+r = result
+print(result)
+
+c = [1,2,3,4,5,6]
+c1 = ["+,-,*,/,^,%"]
+for item in c :
+    if item in c[0]:
+        print("+")
+    if item in c[0]+1:
+       print(c1.index[0]+1)
+
+
+
+'#'from math import*
+c2 = [7,8,9,10]
+c3 = ["m,m1,m1,m3"]
+m = round(n)
+m1 = float(n)
+m2 = ceil(n)
+m3 = "Exit"
+print("Exit")
